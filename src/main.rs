9fe920660051290d03c0cc723feaf936e3a45e72
Boss Tea <almontejosc@gmail.com>
2025-08-22 17:39:07 +0800

    Guessing Game finished

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main () {
-    println!("A guessing game")
-    
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The Guessing Game");
+    println!("Secret number is {secret_number}");
+    loop {
+        let mut guess = String::new();
+        println!("Please guess a number");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read user's input");
+        let guess: u32 = guess.trim().parse().expect("Input a number pls");
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("You Win!!");
+                break;}
+        }
+    }
 }
